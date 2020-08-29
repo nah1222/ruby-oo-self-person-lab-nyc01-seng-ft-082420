aa93c04ddf28a76fd006317599d03fc4e0888d2c
Nicholas Ho <nah1222@yahoo.com>
2020-08-29 18:21:42 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,71 @@
 # your code goes here
+require 'pry'
+class Person 
+
+    attr_accessor :bank_account, :happiness, :hygiene
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(num)
+        @happiness = num
+        @happiness = 10 if @happiness > 10
+        @happiness = 0 if @happiness < 0
+    end
+
+    def hygiene=(num)
+        @hygiene = num
+        @hygiene = 10 if @hygiene > 10
+        @hygiene = 0 if @hygiene < 0
+    end
+    def happy?
+        if @happiness > 7
+            true
+        else
+            false
+        end
+    end
+    def clean?
+        if @hygiene > 7
+            true
+        else
+            false
+        end
+    end
+    def get_paid(salary)
+        @bank_account += salary
+        "all about the benjamins"
+    end
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+
+    end
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        "♪ another one bites the dust ♫"
+    end
+    def call_friend(friend)
+        [friend, self].each {|ele| ele.happiness += 3 }
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+    def start_conversation(person, topic)
+        element = [self, person]
+        if topic == "politics"
+          element.each { |ele| ele.happiness -= 2}
+          return 'blah blah partisan blah lobbyist'
+        elsif topic == "weather"
+          element.each { |ele| ele.happiness += 1}
+          return 'blah blah sun blah rain'
+        end
+        "blah blah blah blah blah"
+      end
+
+
+end
